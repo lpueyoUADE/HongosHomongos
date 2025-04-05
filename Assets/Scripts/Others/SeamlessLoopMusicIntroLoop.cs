3a554833ee229feb373e4cb3a7cfc9fea64e2e7e
Arielga <81482233+a7xpadd3r@users.noreply.github.com>
2025-04-05 20:16:14 -0300

    Removed hiding Main menu buttons

--- Assets/Scripts/Others/SeamlessLoopMusicIntroLoop.cs
+++ Assets/Scripts/Others/SeamlessLoopMusicIntroLoop.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class SeamlessLoopMusicIntroLoop : MonoBehaviour
 {
-    public GameObject mainMenuObject;
     public AudioSource audio1;
     public AudioSource audio2;
 
@@ -16,7 +15,6 @@ public class SeamlessLoopMusicIntroLoop : MonoBehaviour
     {
         audio1.Play();
         yield return new WaitUntil(() => audio1.time >= audio1.clip.length);
-        mainMenuObject.SetActive(true);
         audio2.Play();
         Destroy(this);
     }
